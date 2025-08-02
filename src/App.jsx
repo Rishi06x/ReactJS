2de883aaa742bdf239cf98a6457655b7578a1d9d
Rishi06x <rishiranjith1234@gmail.com>
2025-08-02 18:28:41 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import Component from './Components/Component.jsx';
-import './App.css';
 function App() {
-  return(
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <Component />
-    </div>
-  )
+
 }
 
 export default App;
