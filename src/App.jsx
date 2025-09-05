69217aad4795aa60f557bc79a8999906b06b9135
Rishi <rishiranjith1234@gmail.com>
2025-09-05 12:14:24 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,11 @@
 // import Counter from "./useMemo/Counter.jsx"
 // import Demo from "./useCallback/Demo.jsx"
 // import Counter from "./CustomHooks/Counter.jsx"
-import ImgGallery from "./ImageGallery/ImgGallery";
+import ImgContainer from "./ImageGallery/ImgContainer";
 function App() {
     
     return (
-        <ImgGallery/>
+        <ImgContainer/>
     );
 }
 
