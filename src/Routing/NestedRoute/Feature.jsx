f37496e0c4b343d4133705999bb6fc5a630c304f
Rishi <rishiranjith1234@gmail.com>
2025-11-15 22:41:21 +0530

    Practiced Nested Routing

---
+++ src/Routing/NestedRoute/Feature.jsx
@@ -0,0 +1,8 @@
+export default function Feature() {
+    return (
+        <div className="bg-amber-100 w-3/4 p-4">
+            <h2 className="text-3xl">Feature Component</h2>
+            <p>This is the Feature component nested under Home route.</p>
+        </div>
+    );
+}
---
+++ src/Routing/NestedRoute/New.jsx
@@ -0,0 +1,8 @@
+export default function New() { 
+    return (
+        <div className="bg-amber-100 w-3/4 p-4">
+            <h2 className="text-3xl">New Component</h2>
+            <p>This is the New component nested under Home route.</p>
+        </div>
+    );
+}
